89df1ff9f356271a9acd70c6f85c085175254f12
feldenserra <michael.serra1@outlook.com>
2023-12-25 23:14:17 -0800

    Added ping pong for testing ipc, need to read further on topic

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 //app controls the application lifecycle - a camelCase module
 // BrowserWindow creates and manages app windows - a PascalCade module
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
+
+// this allows us to have global scope for preloading
 const path = require('node:path')
 
 const createWindow = () => {
@@ -9,6 +11,7 @@ const createWindow = () => {
         height: 600,
         webPreferences: {
             preload: path.join(__dirname,'preload.js')
+            //the path.join gets the root and combines it with our preload.js, loading it alongside the renderer creation
         }
     })
 
@@ -17,9 +20,10 @@ const createWindow = () => {
 
 /* In electron BrowserWindows can only be created after the app ready event is fired. using app.whenReady() and calling createWindow() is ideal to ensure promise completion. Typically in JS you use the .on function to listen, but the .whenready avoids certain issues, link: https://github.com/electron/electron/pull/21972 */
 
-app.whenReady().then(()=> {
+app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong')
     createWindow()
-
+    
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow()
--- preload.js
+++ preload.js
@@ -1,8 +1,16 @@
-const { contextBridge } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron')
+
+//context bridge creates a safe bi directional, synchronous bridge across isolated contexts
+// https://www.electronjs.org/docs/latest/api/context-bridge
 
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron
+  electron: () => process.versions.electron,
   // we can also expose variables, not just functions
+  ping: () => ipcRenderer.invoke('ping')
 })
+
+/*
+Notice how we wrap the ipcRenderer.invoke('ping') call in a helper function rather than expose the ipcRenderer module directly via context bridge. You never want to directly expose the entire ipcRenderer module via preload. This would give your renderer the ability to send arbitrary IPC messages to the main process, which becomes a powerful attack vector for malicious code.
+*/
