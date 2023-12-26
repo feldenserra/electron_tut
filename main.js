4e192bbc5de6f3ac3e27241f422f41047cd25111
feldenserra <michael.serra1@outlook.com>
2023-12-25 22:04:45 -0800

    Added base code to open window to main.js, verified working

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-console.log('Hello World!')
+/* Test for console print
+console.log('Hello World!') */
+
+//app controls the application lifecycle - a camelCase module
+// BrowserWindow creates and manages app windows - a PascalCade module
+const {app, BrowserWindow} = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    win.loadFile('index.html')
+}
+
+/* In electron BrowserWindows can only be created after the app ready event is fired. using app.whenReady() and calling createWindow() is ideal to ensure promise completion. Typically in JS you use the .on function to listen, but the .whenready avoids certain issues, link: https://github.com/electron/electron/pull/21972 */
+
+app.whenReady().then(()=> {
+    createWindow()
+})
 
